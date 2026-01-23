86c2a8420dfa38b32767bbe2f7e5f624b3ee4fc1
jack <1506289682@qq.com>
2026-01-23 11:51:21 +0800

    fix(docs): 调整目标TCP姿态和等待运动结束的轮询间隔

--- pyAgxArm/demos/piper_x/test1.py
+++ pyAgxArm/demos/piper_x/test1.py
@@ -1,10 +1,10 @@
 import time
-from pyAgxArm.api.agx_arm_factory import create_agx_arm_config, AgxArmFactory
+from pyAgxArm import create_agx_arm_config, AgxArmFactory
 
 
 def wait_motion_done(robot, timeout: float = 5.0, poll_interval: float = 0.1) -> bool:
     """Wait until `robot.get_arm_status().msg.motion_status == 0` or timeout."""
-    time.sleep(0.1)
+    time.sleep(0.5)
     start_t = time.monotonic()
     while True:
         status = robot.get_arm_status()
@@ -47,26 +47,21 @@ print(end_effector.__doc__)
 # -------------------------- Move ----------------------------
 
 # robot.move_p([0.1, 0.0, 0.3, -1.4836869438278597, 0.0, -1.5707963267948966])
-# time.sleep(0.1)
 # wait_motion_done(robot, timeout=5.0)
 
 # robot.move_l([0.2, 0.0, 0.3, -1.4836869438278597, 0.0, -1.5707963267948966])
-# time.sleep(0.1)
 # wait_motion_done(robot, timeout=5.0)
 
 # start_pose = [0.2, 0.0, 0.3, -1.4836869438278597, 0.0, -1.5707963267948966]
 # mid_pose = [0.2, 0.05, 0.35, -1.4836869438278597, 0.0, -1.5707963267948966]
 # end_pose = [0.2, 0.0, 0.4, -1.4836869438278597, 0.0, -1.5707963267948966]
 # robot.move_c(start_pose, mid_pose, end_pose)
-# time.sleep(0.1)
 # wait_motion_done(robot, timeout=5.0)
 
 # robot.move_j([0.0, 0.4, -0.4, -0.4, 0, 0])
-# time.sleep(0.1)
 # wait_motion_done(robot, timeout=5.0)
 
 # robot.move_j([0] * 6)
-# time.sleep(0.1)
 # wait_motion_done(robot, timeout=5.0)
 
 
